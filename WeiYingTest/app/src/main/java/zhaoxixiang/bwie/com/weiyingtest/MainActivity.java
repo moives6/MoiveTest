cf0b285f72bc213c3eb0824b6e43ad173b7a09c2
zxx <369579262@qq.com>
2017-12-13 20:57:47 +0800

    11

--- WeiYingTest/app/src/main/java/zhaoxixiang/bwie/com/weiyingtest/MainActivity.java
+++ WeiYingTest/app/src/main/java/zhaoxixiang/bwie/com/weiyingtest/MainActivity.java
@@ -1,16 +1,25 @@
 package zhaoxixiang.bwie.com.weiyingtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hjm.bottomtabbar.BottomTabBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomTabBar mainTabBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initView();
 
 
+    }
 
+    private void initView() {
+        mainTabBar = (BottomTabBar) findViewById(R.id.main_tabBar);
     }
 }
